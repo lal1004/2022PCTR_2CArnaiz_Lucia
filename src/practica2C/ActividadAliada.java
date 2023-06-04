9dc97a620c20c4422f37f811fb24b14d883b8ed8
Lucía Arnaiz López <luciaa200120@gmail.com>
2023-06-04 21:17:36 +0200

    ActividadAliada mas o menos completada

--- src/practica2C/ActividadAliada.java
+++ src/practica2C/ActividadAliada.java
@@ -7,11 +7,29 @@ import java.util.logging.Logger;
 
 public class ActividadAliada implements Runnable {
 
+	private int M;//número máximo de enemigos al mismo tiempo
+	private int tipoEnemigo;
+	private IJuego juego;
+	
+	public ActividadAliada(int tipoEnemigo,IJuego juego) {
+		this.tipoEnemigo = tipoEnemigo;
+		this.juego = juego;
+	}
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		Random aleatorio = new Random();
+		for (int i = 0; i < M; i ++) {
+			try {
+				juego.eliminarEnemigo(tipoEnemigo);
+				int tiempoSleep = aleatorio.nextInt(5) * 1000;
+				TimeUnit.MILLISECONDS.sleep(tiempoSleep);
+			}catch (InterruptedException e) {
+				Logger.getGlobal().log(Level.INFO, "Interrumpe la generación de enemigos");
+				Logger.getGlobal().log(Level.INFO, e.toString());
+				return;
+			}
+		}
 	}
-	
-		
 }
--- src/practica2C/SistemaLanzador.java
+++ src/practica2C/SistemaLanzador.java
@@ -1,5 +1,15 @@
 package practica2C;
 
-public class SistemaLanzador {
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class SistemaLanzador {
+	
+	 public static void main(String[] args) {
+		 
+		 
+		 
+	 }
 }
